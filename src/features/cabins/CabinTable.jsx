08e82764747b28d8bd6740fb4bdbf5fa765e445e
RehmanAli10 <vrehmanali@gmail.com>
2024-03-21 23:05:38 -0700

    created reuseable table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,18 +1,9 @@
 import styled from "styled-components";
-import { useQuery } from "@tanstack/react-query";
-import { getCabins } from "../../services/apiCabins";
+
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
-
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
-
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
+import Table from "../../ui/Table";
 
 const TableHeader = styled.header`
   display: grid;
@@ -35,18 +26,22 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table role="table">
-      <TableHeader role="row">
+    <Table columns={"0.6fr 1.8fr 2.2fr 1fr 1fr 1fr"}>
+      <Table.Header role="row">
         <div></div>
         <div>Cabin</div>
         <div>Capacity</div>
         <div>Price</div>
         <div>Discount</div>
         <div></div>
-      </TableHeader>
-      {cabins.map((currCabin) => (
-        <CabinRow cabin={currCabin} key={currCabin.id} />
-      ))}
+      </Table.Header>
+
+      <Table.Body
+        data={cabins}
+        render={(currCabin) => (
+          <CabinRow cabin={currCabin} key={currCabin.id} />
+        )}
+      />
     </Table>
   );
 }
