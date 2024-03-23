c041c97b854dd1e0ee51c01002e74f197282a9ba
RehmanAli10 <vrehmanali@gmail.com>
2024-03-23 06:16:35 -0700

    removed not used styling

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,30 +1,36 @@
-import styled from "styled-components";
-
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  // FILTER
+  const filterdValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+
+  if (filterdValue === "all") filteredCabins = cabins;
+
+  if (filterdValue === "no-discount")
+    filteredCabins = cabins.filter((currCabin) => currCabin.discount === 0);
+
+  if (filterdValue === "with-discount")
+    filteredCabins = cabins.filter((currCabin) => currCabin.discount > 0);
+
+  // SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -39,7 +45,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={sortedCabins}
           render={(currCabin) => (
             <CabinRow cabin={currCabin} key={currCabin.id} />
           )}
