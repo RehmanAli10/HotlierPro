e58e36cbf6486a2ccf09fe97914bfd7e0a386481
RehmanAli10 <vrehmanali@gmail.com>
2024-03-29 10:49:10 -0700

    booking details of selected booking

--- src/features/bookings/BookingDataBox.jsx
+++ src/features/bookings/BookingDataBox.jsx
@@ -1,27 +1,30 @@
 import styled from "styled-components";
 import { format } from "date-fns";
 
-import { box } from "styles/styles";
-import { formatDistanceFromNow } from "utils/helpers";
-import { isToday } from "date-fns/esm";
-import { formatCurrency } from "utils/helpers";
+// import { box } from "styles/styles";
+import { formatDistanceFromNow } from "../../utils/helpers";
+import { isToday } from "date-fns";
+import { formatCurrency } from "../../utils/helpers";
 import {
   HiOutlineChatBubbleBottomCenterText,
   HiOutlineCheckCircle,
   HiOutlineCurrencyDollar,
   HiOutlineHomeModern,
 } from "react-icons/hi2";
-import DataItem from "ui/DataItem";
-import { Flag } from "ui/Flag";
+import DataItem from "../../ui/DataItem";
+import { Flag } from "../../ui/Flag";
 
 const StyledBookingDataBox = styled.section`
-  ${box} /* padding: 3.2rem 4rem; */
+  /* Box */
+  background-color: var(--color-grey-0);
+  border: 1px solid var(--color-grey-100);
+  border-radius: var(--border-radius-md);
+
   overflow: hidden;
 `;
 
 const Header = styled.header`
   background-color: var(--color-brand-500);
-  /* padding: 2.4rem 4rem; */
   padding: 2rem 4rem;
   color: #e0e7ff;
   font-size: 1.8rem;
--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,17 +1,16 @@
 import styled from "styled-components";
 
-// // import Spinner from "../../ui/Spinner";
-// // import BookingDataBox from "./BookingDataBox";
 import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
 import Tag from "../../ui/Tag";
-// import ButtonGroup from "../../ui/ButtonGroup";
-// import Button from "../../ui/Button";
-// import Modal from "../../ui/Modal";
+import ButtonGroup from "../../ui/ButtonGroup";
+import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
+import Spinner from "../../ui/Spinner";
 
+import BookingDataBox from "./BookingDataBox";
 import { useMoveBack } from "../../hooks/useMoveBack";
-// import ConfirmDelete from "../../ui/ConfirmDelete";
+import { useBooking } from "./useBooking";
 
 // const HeadingGroup = styled.div`
 //   display: flex;
@@ -84,11 +83,13 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
-  const bookings = {};
-
-  const status = "checked-in";
+  const { booking, isLoading } = useBooking();
 
   const moveBack = useMoveBack();
+
+  if (isLoading) return <Spinner />;
+
+  const { status, id: bookingId } = booking;
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -100,11 +101,22 @@ function BookingDetail() {
     <>
       <Row>
         <HeadingGroup>
-          <Heading type="h1">Booking #X</Heading>
+          <Heading type="h1">Booking #{bookingId}</Heading>
           <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
+
+      <BookingDataBox booking={booking} />
+
+      <ButtonGroup>
+        {status === "unconfirmed" && <Button>Check in</Button>}
+        {status === "checked-in" && <Button>Check out</Button>}
+
+        <Button variation="secondary" onClick={moveBack}>
+          Back
+        </Button>
+      </ButtonGroup>
     </>
   );
 }
