7256689c235e24fc2707b0db78b5c6738129b8fa
RehmanAli10 <vrehmanali@gmail.com>
2024-04-11 10:01:22 -0700

    created form for updating user data

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,11 +1,11 @@
-import { useUser } from 'features/authentication/useUser';
-import { useState } from 'react';
-import Button from 'ui/Button';
-import FileInput from 'ui/FileInput';
-import Form from 'ui/Form';
-import FormRow from 'ui/FormRow';
-import Input from 'ui/Input';
-import { useUpdateUser } from './useUpdateUser';
+import { useUser } from "../../features/authentication/useUser";
+import { useState } from "react";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import { useUpdateUser } from "./useUpdateUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state
@@ -16,25 +16,14 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
+  const { updateUser, isLoading } = useUpdateUser();
+
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
-  const { mutate: updateUser, isLoading: isUpdating } = useUpdateUser();
-
   function handleSubmit(e) {
     e.preventDefault();
-    if (!fullName) return;
-
-    updateUser(
-      { fullName, avatar },
-      {
-        onSuccess: () => {
-          setAvatar(null);
-          // Resetting form using .reset() that's available on all HTML form elements, otherwise the old filename will stay displayed in the UI
-          e.target.reset();
-        },
-      }
-    );
+    updateUser({ fullName, avatar });
   }
 
   function handleCancel(e) {
@@ -45,32 +34,30 @@ function UpdateUserDataForm() {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRow label='Email address'>
+      <FormRow label="Email address">
         <Input value={email} disabled />
       </FormRow>
-      <FormRow label='Full name'>
+      <FormRow label="Full name">
         <Input
-          type='text'
+          type="text"
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
-          disabled={isUpdating}
-          id='fullName'
+          id="fullName"
         />
       </FormRow>
-      <FormRow label='Avatar image'>
+      <FormRow label="Avatar image">
         <FileInput
-          disabled={isUpdating}
-          id='avatar'
-          accept='image/*'
+          id="avatar"
+          accept="image/*"
           onChange={(e) => setAvatar(e.target.files[0])}
           // We should also validate that it's actually an image, but never mind
         />
       </FormRow>
       <FormRow>
-        <Button onClick={handleCancel} type='reset' variation='secondary'>
+        <Button onClick={handleCancel} type="reset" variation="secondary">
           Cancel
         </Button>
-        <Button disabled={isUpdating}>Update account</Button>
+        <Button>Update account</Button>
       </FormRow>
     </Form>
   );
