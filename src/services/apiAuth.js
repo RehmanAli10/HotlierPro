d476658a950710e38f06f3b3ac3e649f9557a0dd
RehmanAli10 <vrehmanali@gmail.com>
2024-04-14 09:06:32 -0700

    created custom hooks

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signupUser({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -42,6 +42,7 @@ export async function getCurrentUser() {
 }
 
 export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1- Update password or fullName
   let updateData;
   if (password) updateData = { password };
 
@@ -59,11 +60,25 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
 
   if (!avatar) return data;
 
+  // 2- Upload avatar image
   const fileName = `avatar-${data.user.id}-${Math.random()}`;
 
-  const { error } = await supabase.storage
+  const { error: storageError } = await supabase.storage
     .from("avatars")
     .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  //3- Upload avatar in the user
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+
+  if (error2) throw new Error(error2.message);
+
+  return updatedUser;
 }
 
 export async function logout() {
